66adad57b9ce99774c70118fe324b17d6edfdc59
mmkm10 <mansimadhushree13@gmail.com>
2021-07-15 01:57:41 +0530

    build

---
+++ build/precache-manifest.21be1713ef0ee5b610b7fad20c7d0e21.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77706030b68e70a8ccdfe384e1d6127a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25508d9964c1fcb20803",
+    "url": "/static/css/2.381f00bd.chunk.css"
+  },
+  {
+    "revision": "f9cc59a6c4c0bb1fdace",
+    "url": "/static/css/main.0cfe6c27.chunk.css"
+  },
+  {
+    "revision": "25508d9964c1fcb20803",
+    "url": "/static/js/2.dc58905f.chunk.js"
+  },
+  {
+    "revision": "b27cfd3ceda6cddc26c7474a86da0684",
+    "url": "/static/js/2.dc58905f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9cc59a6c4c0bb1fdace",
+    "url": "/static/js/main.6983a0df.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/static/media/mimi9.b5ec7c32.gif"
+  }
+]);
